2c94df072875230e62fcb4f34159fced92d66de0
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-01-17 20:25:26 +0530

    Add sign up view

---
+++ src/main/java/tech/sushnag22/doghouse/ui/views/SignUpView.java
@@ -0,0 +1,34 @@
+package tech.sushnag22.doghouse.ui.views;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import tech.sushnag22.doghouse.backend.service.UserService;
+import tech.sushnag22.doghouse.ui.components.SignUpForm;
+
+@Route("registration")
+public class SignUpView extends VerticalLayout {
+
+    private UserService userService;
+
+    public SignUpView(UserService userService) {
+        this.userService = userService;
+        SignUpForm signUpForm = new SignUpForm(userService);
+        H2 header = new H2("Register");
+        HorizontalLayout horizontalLayout = new HorizontalLayout(header, signUpForm);
+
+        horizontalLayout.setWidthFull();
+        horizontalLayout.setAlignItems(Alignment.CENTER);
+        horizontalLayout.setJustifyContentMode(JustifyContentMode.CENTER);
+
+        setHeightFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setHorizontalComponentAlignment(Alignment.CENTER, header);
+
+        add(header, horizontalLayout);
+
+    }
+
+}
