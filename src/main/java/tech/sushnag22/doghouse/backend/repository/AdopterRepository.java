c4deabf592c438e14697cc7e0fd3ed47ad470ee1
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-01-18 17:17:55 +0530

    Add adopter repository

---
+++ src/main/java/tech/sushnag22/doghouse/backend/repository/AdopterRepository.java
@@ -0,0 +1,12 @@
+package tech.sushnag22.doghouse.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.sushnag22.doghouse.backend.entity.Adopter;
+
+import java.util.List;
+
+public interface AdopterRepository extends JpaRepository<Adopter, Long> {
+    List<Adopter> findByNameContainsIgnoreCase(String name);
+
+    List<Adopter> findByGender(Character gender);
+}
