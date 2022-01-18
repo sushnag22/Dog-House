4a4aba6143904cd7b507b74094491f9a796cdff4
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-01-18 23:24:35 +0530

    Add servlet initializer

---
+++ src/main/java/tech/sushnag22/doghouse/ServletInitializer.java
@@ -0,0 +1,13 @@
+package tech.sushnag22.doghouse;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DogHouseApplication.class);
+    }
+
+}
