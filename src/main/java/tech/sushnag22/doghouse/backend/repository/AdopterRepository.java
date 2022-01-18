c599e875f5bae883a1372cfc7ed495d1da1f74da
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-01-18 19:09:43 +0530

    Update adopter repository

--- src/main/java/tech/sushnag22/doghouse/backend/repository/AdopterRepository.java
+++ src/main/java/tech/sushnag22/doghouse/backend/repository/AdopterRepository.java
@@ -6,7 +6,5 @@ import tech.sushnag22.doghouse.backend.entity.Adopter;
 import java.util.List;
 
 public interface AdopterRepository extends JpaRepository<Adopter, Long> {
-    List<Adopter> findByNameContainsIgnoreCase(String name);
-
-    List<Adopter> findByGender(Character gender);
+    List<Adopter> findByFirstNameContainsIgnoreCase(String name);
 }
