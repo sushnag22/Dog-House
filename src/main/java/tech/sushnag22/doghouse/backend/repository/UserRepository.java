be4d51df2abcf24a0f073869fc3424cea959a994
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-02-10 11:36:48 +0530

    Update user repository

--- src/main/java/tech/sushnag22/doghouse/backend/repository/UserRepository.java
+++ src/main/java/tech/sushnag22/doghouse/backend/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package tech.sushnag22.doghouse.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 import tech.sushnag22.doghouse.backend.entity.Adopter;
 import tech.sushnag22.doghouse.backend.entity.User;
 
@@ -8,4 +9,6 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     List<Adopter> findByUsernameContainsIgnoreCase(String name);
+    @Procedure(name = "findAllUsers")
+    void findAllUsers();
 }
