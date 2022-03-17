e41744625df1b816fd7ab1c10fa0d35ffc8c5558
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-03-17 08:45:18 +0530

    Update dog view

--- src/main/java/tech/sushnag22/doghouse/ui/views/dog/DogView.java
+++ src/main/java/tech/sushnag22/doghouse/ui/views/dog/DogView.java
@@ -2,6 +2,7 @@ package tech.sushnag22.doghouse.ui.views.dog;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -14,44 +15,44 @@ import tech.sushnag22.doghouse.ui.MainLayout;
 @Route(value = "edit-dog", layout = MainLayout.class)
 @PageTitle("Edit Dog Details")
 public class DogView extends VerticalLayout {
-
+    private final DogRepository dogRepository;
     private final TextField textField;
     private final Grid<Dog> grid;
     private final Button newButton;
-    private final DogRepository dogRepository;
     private final DogEditView dogEditView;
 
-    public DogView(DogRepository dogRepository, DogEditView dogEditView) {
+    public DogView(DogRepository dogRepository, DogEditView dogEditView){
         this.dogRepository = dogRepository;
         this.dogEditView = dogEditView;
 
         this.textField = new TextField("Dog Name: ");
         this.textField.setValueChangeMode(ValueChangeMode.EAGER);
-        this.textField.addValueChangeListener(e -> refreshGrid(e.getValue()));
+        this.textField.addValueChangeListener(e -> this.refreshGrid(e.getValue()));
 
         this.grid = new Grid<>(Dog.class);
         this.grid.setColumns("name", "birthDate", "gender", "colour", "description", "location");
         this.grid.asSingleSelect().addValueChangeListener(e -> dogEditView.setDog(e.getValue()));
         this.refreshGrid(null);
 
-        this.newButton = new Button("New Dog");
-        this.newButton.addClickListener(e -> dogEditView.setDog(new Dog()));
+        this.newButton = new Button("New Dog", VaadinIcon.PLUS.create());
+        this.newButton.addClickListener(e -> this.dogEditView.setDog(new Dog()));
 
         this.dogEditView.setDogEditViewHandler(() -> {
-            this.refreshGrid(textField.getValue());
+            this.refreshGrid(this.textField.getValue());
             this.dogEditView.setVisible(false);
+            this.dogEditView.setDog(null);
         });
 
-        this.add(textField);
-        this.add(grid);
-        this.add(newButton);
-        this.add(dogEditView);
+        this.add(this.textField);
+        this.add(this.grid);
+        this.add(this.newButton);
+        this.add(this.dogEditView);
     }
 
-    private void refreshGrid(String name) {
-        grid.setItems(
-                name != null && !"".equals(name.trim()) ?
-                        dogRepository.findByNameContainsIgnoreCase(name)
+    private void refreshGrid(String filter){
+        this.grid.setItems(
+                filter != null && !"".equals(filter.trim())?
+                        dogRepository.findByNameContainsIgnoreCase(filter)
                         :
                         dogRepository.findAll()
         );
