511ea43daddfb3758081ff9cdce0b962099d209f
Sushruth Nagaraj <sushruthnagaraj.github@gmail.com>
2022-03-26 11:46:11 +0530

    Update breed repository

--- src/main/java/tech/sushnag22/doghouse/backend/repository/BreedRepository.java
+++ src/main/java/tech/sushnag22/doghouse/backend/repository/BreedRepository.java
@@ -1,10 +1,13 @@
 package tech.sushnag22.doghouse.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.query.Procedure;
 import tech.sushnag22.doghouse.backend.entity.Breed;
 
 import java.util.List;
 
 public interface BreedRepository extends JpaRepository<Breed, Long> {
+    @Procedure(procedureName = "findBreedByName")
     List<Breed> findByNameContainsIgnoreCase(String name);
+    void findBreedByName(String name);
 }
